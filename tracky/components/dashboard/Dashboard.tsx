e37e2553bc8b04b066d97b45855487f848024d4f
gergokoncz <gergokoncz06@gmail.com>
2022-10-04 18:01:44 +0200

    some cleaning, some adding tailwind bootstrap d3

---
+++ tracky/components/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+    </Container>
+  );
+};
+
+export default Dashboard;
---
+++ tracky/pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+// next imports
+import { NextPage } from "next";
+
+// custom component
+import Dashboard from "../../components/dashboard/Dashboard";
+
+const DashboardPage: NextPage = () => {
+  return <Dashboard />;
+};
+
+export default DashboardPage;
