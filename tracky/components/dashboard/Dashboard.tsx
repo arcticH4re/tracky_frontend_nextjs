bc7ff2c9187b8c0966bf794b1f9b9cfcceb9c5eb
blankapadar <blanka.padr@gmail.com>
2022-11-27 20:12:07 +0100

    Revert "removed charts"
    This reverts commit 4f7fb5614e966cac2e6d639e4ea8b6cff84d852e.

--- tracky/components/dashboard/Dashboard.tsx
+++ tracky/components/dashboard/Dashboard.tsx
@@ -1,28 +1,44 @@
 import { Container } from "react-bootstrap";
 import DashboardForm from "./DashboardForm";
 import React, { useState } from "react";
+import { Line } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
+
+// import {
+//   Charts,
+//   ChartContainer,
+//   ChartRow,
+//   YAxis,
+//   LineChart,
+// } from "react-timeseries-charts";
+
+Chart.register(ArcElement);
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 import Chart from "chart.js/auto";
 
 const WATER = [
   {
     amount: 0.5,
-    date: new Date(2022, 3, 3),
-  },
-  {
-    amount: 65.62,
-    date: new Date(2022, 3, 3),
-  },
-  {
-    amount: 67.01,
-    date: new Date(2022, 3, 3),
-  },
-  {
-    amount: 68.1,
-    date: new Date(2022, 3, 3),
-  },
-  {
-    amount: 69.45,
     date: new Date(2022, 3, 3),
   },
 ];
@@ -32,43 +48,11 @@ const WARM_WATER = [
     amount: 0.4,
     date: new Date(2022, 11, 28),
   },
-  {
-    amount: 36.91,
-    date: new Date(2022, 11, 28),
-  },
-  {
-    amount: 37.45,
-    date: new Date(2022, 11, 28),
-  },
-  {
-    amount: 37.84,
-    date: new Date(2022, 11, 28),
-  },
-  {
-    amount: 38.27,
-    date: new Date(2022, 11, 28),
-  },
 ];
 
 const HEATING = [
   {
     amount: 57.1,
-    date: new Date(2022, 5, 13),
-  },
-  {
-    amount: 2523.6,
-    date: new Date(2022, 5, 13),
-  },
-  {
-    amount: 2523.6,
-    date: new Date(2022, 5, 13),
-  },
-  {
-    amount: 2523.6,
-    date: new Date(2022, 5, 13),
-  },
-  {
-    amount: 2523.6,
     date: new Date(2022, 5, 13),
   },
 ];
@@ -81,6 +65,48 @@ const Dashboard: React.FC = () => {
   const waterArray = [0.5, 65.62, 67.01, 68.1, 69.45];
   const warmWaterArray = [0.4, 36.91, 37.45, 37.84, 38.27];
   const heatingArray = [57.1, 2523.6, 2523.6, 2523.6, 2523.6];
+
+  const labelArray = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const chartData = {
+    labels: labelArray,
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: waterArray,
+        fill: true,
+        borderColor: "rgb(1335, 102, 132)",
+        tension: 0.1,
+      },
+      {
+        label: "My First Dataset",
+        data: warmWaterArray,
+        fill: true,
+        borderColor: "rgb(95, 22, 192)",
+        tension: 0.1,
+      },
+      {
+        label: "My First Dataset",
+        data: heatingArray,
+        fill: true,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  };
 
   const submitDataHandler = (submittedFormData) => {
     const formData = {
@@ -128,6 +154,18 @@ const Dashboard: React.FC = () => {
     }
   };
 
+  // const data = {
+  //   name: "trafficc",
+  //   columns: ["time", "value"],
+  //   points: [
+  //     [1400425947000, 52],
+  //     [1400425948000, 18],
+  //     [1400425949000, 26],
+  //     [1400425950000, 93],
+  //   ],
+  // };
+  // const timerange = new TimeSeries(data);
+
   return (
     <Container className="bg-white">
       <div>
@@ -164,6 +202,21 @@ const Dashboard: React.FC = () => {
         </div>
       </div>
       <DashboardForm onSubmitData={submitDataHandler} />
+      {/* <ChartContainer timeRange={timerange}>
+        <ChartRow height="200">
+          <YAxis id="y" label="Price ($)" min={0.5} max={1.5} format="$,.2f" />
+          <Charts>
+            <LineChart
+              axis="y"
+              breakLine={false}
+              series={waterArray}
+              columns={["aud", "euro"]}
+              interpolation="curveBasis"
+            />
+          </Charts>
+        </ChartRow>
+      </ChartContainer> */}
+      <Line data={chartData} className="max-w-lg" />
     </Container>
   );
 };
