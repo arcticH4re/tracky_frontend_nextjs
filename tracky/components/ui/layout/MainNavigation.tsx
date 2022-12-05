ca038267f68a048ec327670067ec2a2bbe8ad953
gergokoncz <gergokoncz06@gmail.com>
2022-12-05 20:17:10 +0100

    most auth working

--- tracky/components/ui/layout/MainNavigation.tsx
+++ tracky/components/ui/layout/MainNavigation.tsx
@@ -1,10 +1,10 @@
 // next imports
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 // react imports
-import { Container, Navbar, Nav } from "react-bootstrap";
-import classes from "../../../styles/MainNavigation.module.css";
+import { Container, Navbar, Nav, Button } from "react-bootstrap";
 
 // urls
 import {
@@ -13,9 +13,24 @@ import {
   DASHBOARD_PAGE,
   CONTACT_PAGE,
   PROFILE_PAGE,
+  LOGIN_PAGE,
 } from "../../../urls/in_app_urls";
 
+// store
+import { useAppDispatch, useAppSelector } from "../../../hooks/store-hooks";
+import { selectIsAuthenticated, logoutUser } from "../../../store/auth";
+
 const MainNavigation = () => {
+  const dispatch = useAppDispatch();
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+
+  const router = useRouter();
+
+  const Logout = () => {
+    dispatch(logoutUser());
+    router.push(HOME_PAGE);
+  };
+  
   return (
     <div className="max-w-full">
       <Navbar bg="dark" variant="dark" expand="sm">
@@ -59,6 +74,15 @@ const MainNavigation = () => {
                 <a className="text-white no-underline">Contact</a>
               </Link>
             </Nav.Item>
+            {!isAuthenticated && (
+              <Nav.Item className="flex items-end text-sm">
+                <Button
+                  className="ml-2 mb-1 text-sm text-white bg-black border-black outline-0"
+                >
+                  <Link href={LOGIN_PAGE}>Login</Link>
+                </Button>
+              </Nav.Item>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
